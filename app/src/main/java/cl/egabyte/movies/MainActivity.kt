5e5bfde5086898c985cf07b27ecdde2940eb715c
👨🏾‍💻 Henry AT <yiperu@gmail.com>
2020-12-27 17:28:08 -0300

    Configure View Binding and show Text

--- app/src/main/java/cl/egabyte/movies/MainActivity.kt
+++ app/src/main/java/cl/egabyte/movies/MainActivity.kt
@@ -2,10 +2,16 @@ package cl.egabyte.movies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import cl.egabyte.movies.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        binding.messageTextView.text = "Hello Android"
+
+
     }
 }
